e0b4b8bf89060cadc23bcd592e104441c3dceec8
Tomasz Sądej <tomasz@medius.pro>
2025-03-18 18:17:43 +0100

    refactor: port forward

--- agma/vpn/init.sh
+++ agma/vpn/init.sh
@@ -4,4 +4,8 @@ mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 chmod 600 /dev/net/tun
 
+iptables -t nat -A PREROUTING -p tcp --dport 31433 -j DNAT --to-destination 10.21.0.205:1433
+iptables -t nat -A PREROUTING -p tcp --dport 31444 -j DNAT --to-destination 10.21.0.222:1433
+iptables -t nat -A POSTROUTING -p tcp -o tun0 -j MASQUERADE --to-ports 11000-21000
+
 exec /usr/sbin/openvpn --config /etc/openvpn/client.ovpn
