338672cb9688a9b769da85a3eb62d30bb7289ba1
Tomasz Sądej <tomasz@medius.pro>
2025-04-30 12:44:55 +0200

    fix: port

--- agma/vpn/init.sh
+++ agma/vpn/init.sh
@@ -6,7 +6,7 @@ chmod 600 /dev/net/tun
 
 iptables -t nat -A PREROUTING -p tcp --dport 31433 -j DNAT --to-destination 10.21.0.205:1433
 iptables -t nat -A PREROUTING -p tcp --dport 31434 -j DNAT --to-destination 10.21.0.222:1433
-iptables -t nat -A PREROUTING -p tcp --dport 31435 -j DNAT --to-destination 10.21.0.222:1433
+iptables -t nat -A PREROUTING -p tcp --dport 31435 -j DNAT --to-destination 10.21.0.208:1433
 iptables -t nat -A POSTROUTING -p tcp -o tun0 -j MASQUERADE --to-ports 11000-21000
 
 exec /usr/sbin/openvpn --config /etc/openvpn/client.ovpn
